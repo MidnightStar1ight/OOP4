73ccb9740955b15405a044e869de80c295ac8f9a
Артём Чепрасов <ceprasovartem@gmail.com>
2024-10-25 01:25:19 +0300

    All in one!

---
+++ mainwindow.h
@@ -0,0 +1,44 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QWidget>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include "polynom.h"
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+    QLabel *nameAnRe, *nameAnIm, *nameRe, *nameIm, *nameEvaluateRe, *nameEvaluateIm, *output, *nameResize;
+    QLineEdit *inputAnRe, *inputAnIm, *inputRe, *inputIm, *changeCoefIndex, *inputEvaluateRe, *inputEvaluateIm, *inputResize;
+    QPushButton *createCoef, *createAn, *showPolynomClassic, *showPolynomCanon, *changeCoefButton, *evaluateButton, *resizeButton;
+
+
+    int
+        setCoefsX = 50,
+        setCoefsY = 50,
+        frameWidth = 210+setCoefsX*2,
+        frameHeight = 340+setCoefsY*2;
+
+    Polynom polynom;
+
+public:
+
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+public slots:
+
+    void setCoef();
+    void setAn();
+    void printPolynomClassic();
+    void printPolynomCanon();
+    void changeCoef();
+    void evaluateSlot();
+    void resizeSlot();
+
+};
+#endif // MAINWINDOW_H
